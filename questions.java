bcd60b2881bc788666894771be80600c07c07d12
Raghav Singh <actualrag@gmail.com>
2023-12-04 00:31:49 +0530

    exercise questions.

---
+++ questions.java
@@ -0,0 +1,12 @@
+public class questions {
+    //For those who wanna practice the questions :)
+
+//    Questions
+//
+//    Enter 3 numbers from the user & make a function to print their average.
+//    Write a function to print the sum of all odd numbers from 1 to n.
+//    Write a function which takes in 2 numbers and returns the greater of those two.
+//    Write a function that takes in the radius as input and returns the circumference of a circle.
+//    Write a function that takes in age as input and returns if that person is eligible to vote or not. A person of age > 18 is eligible to vote.
+
+}
